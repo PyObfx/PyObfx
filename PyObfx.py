d3732f475ee4adb174b691c29d7f75f051f38321
Efe Aydın <efeaydin06@gmail.com>
2018-09-09 21:29:08 +0300

    main file edited for obfuscator support

--- PyObfx.py
+++ PyObfx.py
@@ -4,21 +4,24 @@
 from scripts.io import read_file
 from scripts.tokenizer import Tokenizer
 from scripts.argparser import function_dispatcher
+from scripts.obfuscator import Obfuscator
 
 # Show cool ascii art and project description
 desc = "PyObfx v1.0"
 
 
 def print_header():
-    print(desc)
+	print(desc)
 
 
 def main():
-    print_header()
-    args = function_dispatcher()
-    pyfile = read_file(args['file'])
-    print(pyfile)  # Print file content for now
-
+	print_header()
+	args = function_dispatcher()
+	pyfile = read_file(args['file'])
+	tokenizer = Tokenizer(pyfile)
+	tokens = tokenizer.tokenize()
+	obfuscator = Obfuscator(pyfile, tokens)
+	obfuscator.obfuscate()
 
 if __name__ == "__main__":
-    main()
+	main()
