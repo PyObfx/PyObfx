bc38614df701257d8b1653385e8c4d7a98ab877b
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-09-18 17:13:39 +0300

    Remove unnecessary layer
    For now, we only have one option which provides the file path for the
    tokenizer. And along with this line, we don't dispatch any other functions from
    the output provided by cli_parser. Also, about custom error messages, when an option
    marked required in parser, it automatically raises its own errors. That means it's
    already handled.

--- scripts/argparser.py
+++ scripts/argparser.py
@@ -1,35 +1,20 @@
 #!/usr/bin/python3.6
 # -*- coding: utf-8 -*-
 import argparse
-"""
-Once, there was a world. A world filled with a magic. A world with lots of
-different races. The world kingdoms wish to conquer. With this greed in their
-eyes, kingdoms'd sacrificed tons of fine warriors, magicians even civillians.
-One day, people heard news from the north. A new kingdom had found. A kingdom 
-that devours everything on its path. They kill people, rape women and raze towns.
-People call them savages. Savages that lost their humanity under their lord. Lord
-Azkward. A man chaos gods chose for their invasion. An invincible man. With the
-power of chaos gods, his invasion's starting from North...
-"""
 
 
 def cli_arg_parser():
+    """
+    Parses the arguments from CLI and returns to the dictionary
+    where keys are the options/flags
+
+    ex: when -f triggered, there'll be 'file' key in the dictionary.
+    and values are accessible with list syntax `[]`
+    """
     parser = argparse.ArgumentParser()
-    # List args
-    # This can be replaced after with -p option that automatically
-    # obsf each .py file in the given -p ath.
     parser.add_argument("-f", "--file",
                         help="File to be obfscated.", required=True)
-    # Dic format
+
+    # Return to dictionary
     return vars(parser.parse_args())
 
-
-def function_dispatcher():
-    args = cli_arg_parser()
-    # Testing purposes. Since [file] is a required param, obviously it wont
-    # be empty.
-    if args['file']:
-        return args
-    return None
-# Testing if its working
-# function_dispatcher()
